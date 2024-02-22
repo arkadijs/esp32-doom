920c3509fc9d1c9cf2d926e7c4882f9aede3c619
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2024-02-23 00:41:28 +0200

    Readme bump

--- flashwad.sh
+++ flashwad.sh
@@ -1,5 +1,5 @@
-#!/bin/sh -x
+#!/bin/sh -xe
 esptool="esptool.py --chip esp32s3 --port /dev/ttyACM0 --baud 921600 --before default_reset --after hard_reset write_flash --flash_mode dio --flash_freq 80m --flash_size 16MB"
 
-$esptool 0x100000 $HOME/doom/wads/doom2.wad
-$esptool 0xF80000 $HOME/doom/wads/prboom-plus.wad
+$esptool 0x100000 $DOOMWADDIR/doom2.wad
+$esptool 0xF80000 $DOOMWADDIR/prboom-plus.wad
